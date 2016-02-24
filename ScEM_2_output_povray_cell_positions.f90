fd57bc359e8ced55488e0767384b1c324202de31
chris-revell <christopher@234.162.248.10.in-addr.arpa.private.cam.ac.uk>
2016-02-24 20:54:09 +0000

    Fixing misnumbered file

--- ScEM_2_output_povray_cell_positions.f90
+++ ScEM_2_output_povray_cell_positions.f90
@@ -21,43 +21,43 @@ module scem_2_output_povray_cell_positions
                                           data/povray_", n_snapshots, ".pov"
 
       !Open file for povray output
-      open(unit=39, file=povray_cells_filename,status='unknown')
+      open(unit=43, file=povray_cells_filename,status='unknown')
 
-      write(39,*) '#version 3.5;'
-      write(39,*) '#include "colors.inc"'
-      write(39,*) '#include "textures.inc"'
-      write(39,*) 'background {White}'
-      write(39,*)
-      write(39,*) 'camera {'
-      write(39,*) '   location  <500, 0, 0>'
-      write(39,*) '   angle 12'
-      write(39,*) '   look_at<0,0,0>}'
-      write(39,*)
-      write(39,*) 'light_source { < -600, 600, 0 > color White }'
-      write(39,*) 'light_source { < 600, -600, 0 > color White }'
-      write(39,*) 'light_source { < 0, 0, 600 > color White }'
-      write(39,*) 'light_source { < 0, 0, -600 > color White }'
-      write(39,*)
+      write(43,*) '#version 3.5;'
+      write(43,*) '#include "colors.inc"'
+      write(43,*) '#include "textures.inc"'
+      write(43,*) 'background {White}'
+      write(43,*)
+      write(43,*) 'camera {'
+      write(43,*) '   location  <500, 0, 0>'
+      write(43,*) '   angle 12'
+      write(43,*) '   look_at<0,0,0>}'
+      write(43,*)
+      write(43,*) 'light_source { < -600, 600, 0 > color White }'
+      write(43,*) 'light_source { < 600, -600, 0 > color White }'
+      write(43,*) 'light_source { < 0, 0, 600 > color White }'
+      write(43,*) 'light_source { < 0, 0, -600 > color White }'
+      write(43,*)
 
       !Draw spheres for all cells in the system, coloured according to cell type
       do i=1, nc
         if ((cells(i)%fate).EQ.1) then
-          write(39,'(A12,F18.14,A2,F18.14,A2,F18.14,A51,I2.2)') ' sphere {  < ', &
+          write(43,'(A12,F18.14,A2,F18.14,A2,F18.14,A51,I2.2)') ' sphere {  < ', &
                         cells(i)%position(1), ',', cells(i)%position(2), &
                           ',', cells(i)%position(3), &
                             '> 10 texture { pigment { color Green } } } // cell ',&
                               cells(i)%label
         else
-          write(39,'(A12,F18.14,A2,F18.14,A2,F18.14,A51,I2.2)') ' sphere {  < ', &
+          write(43,'(A12,F18.14,A2,F18.14,A2,F18.14,A51,I2.2)') ' sphere {  < ', &
                         cells(i)%position(1), ',', cells(i)%position(2), &
                           ',', cells(i)%position(3), &
                             '> 10 texture { pigment { color Red } } } // cell ',&
                               cells(i)%label
         endif
-        write(39,*)
+        write(43,*)
       enddo
 
-      close(unit=39)
+      close(unit=43)
 
     end subroutine scem_output_povray_cell_positions
 
