8d1ce8c7c7bc5a4e9cdd96dc1f2f58260171a2a8
chris-revell <christopher@cr395.cai.private.cam.ac.uk>
2016-02-26 10:53:17 +0000

    Starting to add cube root function

---
+++ cube_root.f90
@@ -0,0 +1,22 @@
+function cube_root
+
+!  public :: cube_root
+
+  implicit none
+
+
+
+  function cube_root(x) !result(root)
+
+  real,intent(in)  :: x
+  real,intent(out) :: cube_root
+  real             :: log_x
+
+  ! Calculate cube root by using logs
+  log_x = log(x)
+
+  cube_root = exp(log_x/3.0)
+
+  end function cube_root
+
+end module cube_root_module
