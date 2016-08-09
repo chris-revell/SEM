e52b50ba1eddb77f5ff6929f30171ed2bb000633
Christopher Revell <cr395@cam.ac.uk>
2016-08-09 11:00:20 +0100

    Setting up new run system, starting at 26 cells

--- SEM/ScEM_0_input.f90
+++ SEM/ScEM_0_input.f90
@@ -177,7 +177,7 @@ module scem_0_input
       diff_coeff=0.001 ! --> diffusion coefficient of elements in units of micron^2/s
 
       ! growth parameters
-      cell_cycle_time=4320			!0.5*3600.0 ! --> cell cycle time in seconds
+      cell_cycle_time=2*4320			!0.5*3600.0 ! --> cell cycle time in seconds
       frac_growth=0.9 ! fraction of current cell radius within which new elements may be placed
       frac_placement_min=0.6 ! minimum separation of new element from nearest neighbour, as fraction of r_equil
         ! derived quantities
@@ -248,7 +248,7 @@ module scem_0_input
       dt_amp_max=dt_amp_max/r_s_max ! rescale dt by largest interaction strength to ensure stable integration
 
       ! temporal parameters - all in *seconds*
-      time_max=6.5*cell_cycle_time ! --> time of simulation in seconds
+      time_max=1.5*cell_cycle_time ! --> time of simulation in seconds
       time_out_1=time_max/99.0 ! --> interval between graphical data outputs, set such that there will be no more than 99 outputs regardless of time_max
 !     time_out_2=cell_cycle_time/100.0 ! --> interval between quantitative data outputs
       dt=dt_amp_max*viscous_timescale_cell/(ne_cell+0.0)**(2*ot) ! --> optimized microscopic time increment
