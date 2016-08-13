0aeb789460025da47515b86d853a7713963dfad9
Christopher Revell <cr395@cam.ac.uk>
2016-08-13 14:03:17 +0100

    DIT at like-like interfaces

--- SEM/ScEM_2_1_DIT.f90
+++ SEM/ScEM_2_1_DIT.f90
@@ -30,7 +30,10 @@ contains
     !This factor is a component of the "elements" data structure and is used
     !as a factor in the calculation of forces on elements.
 
-    !No need to refresh DIT_factor at each timestep as the loop over all pairs guarantees to update every element
+    !Refresh DIT_factor values for all elements
+    do j=1, ne
+      elements(j)%DIT_factor = .FALSE.
+    enddo
 
     do j=1,np
       cell_1  = elements(pairs(j,1))%parent
@@ -41,30 +44,31 @@ contains
         !Do nothing for pairs within the same cell
         CYCLE
       elseif (fate_1.EQ.fate_2) then
-        elements(pairs(j,1))%DIT_factor = .FALSE.
-        elements(pairs(j,2))%DIT_factor = .FALSE.
-      else
         !Set DIT_factor to be .TRUE. for elements only if both elements are in different cells
-        !and those cells have different fates. This routine naturally excludes elements in the same
-        !cell because in this case the fate will always be the same.
+        !and those cells have the same fate.
         elements(pairs(j,1))%DIT_factor = .TRUE.
         elements(pairs(j,2))%DIT_factor = .TRUE.
+      else
+        elements(pairs(j,1))%DIT_factor = .FALSE.
+        elements(pairs(j,2))%DIT_factor = .FALSE.
       endif
     enddo
 
     do j=1, np_cortex
       if (elements(pairs_cortex(j)%label1)%DIT_factor.AND.elements(pairs_cortex(j)%label2)%DIT_factor) then
 !      if (elements(pairs_cortex(j)%label1)%DIT_factor.OR.elements(pairs_cortex(j)%label2)%DIT_factor) then
-!        pairs_cortex(j)%cortex_factor = cells(elements(pairs_cortex(j)%label1)%parent)%fate**2 !DIT response equal to fate. Quick and dirty way to give fate dependence.
-!        pairs_cortex(j)%cortex_factor = 4 !No fate dependence.
 
         !Change cortical tension only for one cell type
-        if (cells(elements(pairs_cortex(j)%label1)%parent)%fate.EQ.1) then
-          call get_command_argument(1,arg1)
-          read(arg1,*) pairs_cortex(j)%cortex_factor
-        else
-          pairs_cortex(j)%cortex_factor = 1.0
-        endif
+!        if (cells(elements(pairs_cortex(j)%label1)%parent)%fate.EQ.1) then
+!          call get_command_argument(1,arg1)
+!          read(arg1,*) pairs_cortex(j)%cortex_factor
+!        else
+!          call get_command_argument(1,arg1)
+!          read(arg1,*) pairs_cortex(j)%cortex_factor
+!        endif
+
+        call get_command_argument(1,arg1)
+        read(arg1,*) pairs_cortex(j)%cortex_factor
 
       else
         pairs_cortex(j)%cortex_factor = 1.0
