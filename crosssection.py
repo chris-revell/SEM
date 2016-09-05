1e80b8ba15f1f4e62d9a63bde9325613de258f95
Christopher Revell <cr395@cam.ac.uk>
2016-09-05 12:50:46 +0100

    Element-based cross section python script

--- crosssection.py
+++ crosssection.py
@@ -1,73 +1,50 @@
 import os
 from sys import argv
+from shutil import copy
 
+importfolderpath = os.path.join(argv[1],"povray_data")
+outputfolderpath = os.path.join(argv[1],"cross_section")
+os.mkdir(outputfolderpath)
 
-importfolderpath = argv[1]
-
+#Find all files in the povray data folder of the run directory passed at the command line
 datafiles = [f for f in os.listdir(importfolderpath) if os.path.isfile(os.path.join(importfolderpath, f)) and f[-4:]=='.pov']
 
-infile1 = open(os.path.join(importfolderpath,datafiles[0]),"r")
-file_lines = infile1.readlines()
+for f in datafiles:
+    #Open files and read lines
+    infile = open(os.path.join(importfolderpath,f),"r")
+    file_lines = infile.readlines()
+    outfile = open(os.path.join(outputfolderpath,f),"w")
 
-cross_section_cells = []
+    cross_section_cells = []
+    for line in file_lines:
+        if "element" in line:
+            linesegment = line[12:70]
+            linesegment = linesegment.replace(" ","")
+            xyzlist = linesegment.split(",")
+            if 0.5 > float(xyzlist[2]) > -0.5:
+                celllabel = "cell"+line.split("cell")[1][0:2]
+                if celllabel in cross_section_cells:
+                    pass
+                else:
+                    cross_section_cells.append(celllabel)
 
-for line in file_lines:
-    if "element" in line:
-        linesegment = line[12:70]
-        linesegment = linesegment.replace(" ","")
-        xyzlist = linesegment.split(",")
-        if 0.5 > float(xyzlist[2]) > -0.5:
+    outfile.write('#version 3.5;\n#include "colors.inc"\n#include "textures.inc"\nbackground {White}\n')
+    outfile.write('camera {location  <0, 0, -500> angle 12 sky <0,0,1> look_at<0,0,0>}\n')
+    outfile.write('light_source { < 0, 0, -60 > color White }\nlight_source { < 60, -60, 0 > color White }\n')
+    outfile.write('light_source { < 0, 0, 60 > color White }\nlight_source { < 0, 0, -60 > color White }\n')
+    for line in file_lines:
+        if "volume" in line:
             celllabel = "cell"+line.split("cell")[1][0:2]
             if celllabel in cross_section_cells:
-                pass
+                outfile.write(line)#.split("transmit")[0]+"}}}")
             else:
-                cross_section_cells.append(celllabel)
-
-print(cross_section_cells)
-outfile1 = open(os.path.join(importfolderpath,"cross_section0.pov"),"w")
-outfile1.write('#version 3.5;\n#include "colors.inc"\n#include "textures.inc"\nbackground {White}\n')
-outfile1.write('camera {location  <0, 0, -500> angle 12 sky <0,0,1> look_at<0,0,0>}\n')
-outfile1.write('light_source { < 0, 0, -60 > color White }\nlight_source { < 60, -60, 0 > color White }\n')
-outfile1.write('light_source { < 0, 0, 60 > color White }\nlight_source { < 0, 0, -60 > color White }\n')
-for line in file_lines:
-    if "volume" in line:
-        celllabel = "cell"+line.split("cell")[1][0:2]
-        if celllabel in cross_section_cells:
-            outfile1.write(line)#.split("transmit")[0]+"}}}")
+                pass
         else:
             pass
-    else:
-        pass
 
-infile2 = open(os.path.join(importfolderpath,datafiles[-1]),"r")
-file_lines = infile2.readlines()
+    infile.close()
+    outfile.close()
 
-cross_section_cells = []
 
-for line in file_lines:
-    if "element" in line:
-        linesegment = line[12:70]
-        linesegment = linesegment.replace(" ","")
-        xyzlist = linesegment.split(",")
-        if 0.5 > float(xyzlist[2]) > -0.5:
-            celllabel = "cell"+line.split("cell")[1][0:2]
-            if celllabel in cross_section_cells:
-                pass
-            else:
-                cross_section_cells.append(celllabel)
-
-print(cross_section_cells)
-outfile2 = open(os.path.join(importfolderpath,"cross_section99.pov"),"w")
-outfile2.write('#version 3.5;\n#include "colors.inc"\n#include "textures.inc"\nbackground {White}\n')
-outfile2.write('camera {location  <0, 0, -500> angle 12 sky <0,0,1> look_at<0,0,0>}\n')
-outfile2.write('light_source { < 0, 0, -60 > color White }\nlight_source { < 60, -60, 0 > color White }\n')
-outfile2.write('light_source { < 0, 0, 60 > color White }\nlight_source { < 0, 0, -60 > color White }\n')
-for line in file_lines:
-    if "volume" in line:
-        celllabel = "cell"+line.split("cell")[1][0:2]
-        if celllabel in cross_section_cells:
-            outfile2.write(line)#.split("transmit")[0]+"}}}")
-        else:
-            pass
-    else:
-        pass
+copy("visualise_povray_script.sh", outputfolderpath)
+os.system("cd outputfolderpath; ./visualise_povray_script.sh")
---
+++ crosssection2.py
@@ -0,0 +1,40 @@
+import os
+from sys import argv
+from shutil import copy
+
+importfolderpath = os.path.join(argv[1],"povray_data")
+outputfolderpath = os.path.join(argv[1],"cross_section")
+os.mkdir(outputfolderpath)
+
+#Find all files in the povray data folder of the run directory passed at the command line
+datafiles = [f for f in os.listdir(importfolderpath) if os.path.isfile(os.path.join(importfolderpath, f)) and f[-4:]=='.pov']
+
+for f in datafiles:
+    #Open files and read lines
+    infile = open(os.path.join(importfolderpath,f),"r")
+    file_lines = infile.readlines()
+    outfile = open(os.path.join(outputfolderpath,f),"w")
+
+    outfile.write('#version 3.5;\n#include "colors.inc"\n#include "textures.inc"\nbackground {White}\n')
+    outfile.write('camera {location  <0, 0, -500> angle 12 sky <0,0,1> look_at<0,0,0>}\n')
+    outfile.write('light_source { < 0, 0, -60 > color White }\nlight_source { < 60, -60, 0 > color White }\n')
+    outfile.write('light_source { < 0, 0, 60 > color White }\nlight_source { < 0, 0, -60 > color White }\n')
+
+    for line in file_lines:
+        if "element" in line:
+            linesegment = line[12:70]
+            linesegment = linesegment.replace(" ","")
+            xyzlist = linesegment.split(",")
+            if 1.0 > float(xyzlist[2]) > -1.0:
+                linehalves = line.split("color ")
+                linehalves[0] = linehalves[0].replace("> 1.5","> 3.0")
+                if "fate1" in line:
+                    outfile.write(linehalves[0]+"Green }}}\n")
+                else:
+                    outfile.write(linehalves[0]+"Red }}}\n")
+
+    infile.close()
+    outfile.close()
+
+
+copy("visualise_povray_script.sh", outputfolderpath)
