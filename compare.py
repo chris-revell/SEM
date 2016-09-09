e0aab700664df23a8a4837429b319fdd6dcc27c7
Christopher Revell <cr395@cam.ac.uk>
2016-09-09 15:02:20 +0100

    Changing bounds in pyplot plots

--- compare.py
+++ compare.py
@@ -28,20 +28,20 @@ for folder in folderstocompare:
 
     #Read data from files in input folder
     radius_array = np.genfromtxt(os.path.join(folder,'mean_radius.txt'))
-    """
+    
     interface_array = np.genfromtxt(os.path.join(folder,'mean_interface.txt'))
     neighbours_array = np.genfromtxt(os.path.join(folder,'mean_neighbours.txt'))
-    """
+
 
     pyplot.figure(1)
     pyplot.plot(radius_array[:,0],radius_array[:,1],label=series_label)
-    """
+
     pyplot.figure(2)
     pyplot.plot(interface_array[:,0],interface_array[:,1],label=components[-1])
 
     pyplot.figure(3)
     pyplot.plot(neighbours_array[:,0],neighbours_array[:,1],label=components[-1])
-    """
+
 
 if os.path.exists(outputfolder):
     pass
@@ -59,7 +59,7 @@ pyplot.ylabel('Average distance of epiblast cells from system centre of mass, \n
 pyplot.title('Comparison of normalised average distance of epiblast cells from\n system centre of mass between systems with different input parameters')
 pyplot.savefig(os.path.join(outputfolder,'radius.pdf'))
 
-"""
+
 pyplot.figure(2)
 lims = pyplot.xlim()
 pyplot.axis([0,lims[1],0,1])
@@ -75,4 +75,3 @@ pyplot.legend(loc='best', shadow=True)
 pyplot.xlabel('Time')
 pyplot.ylabel('Ratio of like-like inter-cell nearest neighbour pairs to \ntotal number of nearest neighbour pairs')
 pyplot.savefig(os.path.join(outputfolder,'neighbours.pdf'))
-"""
