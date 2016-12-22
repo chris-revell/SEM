5295238c14c6800626e38cb74da37f4e7f84103a
Christopher Revell <cr395@cam.ac.uk>
2016-12-22 10:27:57 +0000

    Updated scem_measure_velocity
    Averaging velocity within each cell

--- SEM/ScEM_2_measure_velocity.f90
+++ SEM/ScEM_2_measure_velocity.f90
@@ -13,9 +13,10 @@ contains
 
   subroutine scem_measure_velocity
 
-    integer              :: parent_fate,n
-    real*8, dimension(3) :: displacement
-    real*8               :: radialvelocity,radius,parent_age
+    integer                           :: parent,parent_fate,n
+    real*8                            :: radialvelocity,radius,age
+    real*8, dimension(3)              :: displacement
+    real*8,allocatable,dimension(:,:) :: cell_velocities
 
     if (randomising) then
       open(unit=37,file=output_folder//'/randomised_data/velocity1.txt', status='unknown', position="append")
@@ -25,19 +26,43 @@ contains
       open(unit=38,file=output_folder//'/sorting_data/velocity2.txt', status='unknown', position="append")
     endif
 
+    if (allocated(cell_velocities)) then
+				if (nc.GT.SIZE(cell_velocities,DIM=1)) then
+					deallocate(cell_velocities)
+					allocate(cell_velocities(nc,2))
+				endif
+		else
+			!Array has not yet been allocated (ie, this is the start of the simulation)
+			allocate(cell_velocities(nc,2))
+		endif
+    cell_velocities(:,:) = 0
+
     do n=1, ne
-      parent_fate = cells(elements(n)%parent)%fate
-      parent_age  = cells(elements(n)%parent)%age
+      parent      = elements(n)%parent
+      parent_fate = cells(parent)%fate
       if (parent_fate.EQ.1) then
         displacement(:) = elements(n)%position(:) - epi_COM(:)
         radius = SQRT(DOT_PRODUCT(displacement,displacement))
         radialvelocity = DOT_PRODUCT(displacement(:)/radius,elements(n)%velocity(:)) !Unit vector from COM to element dotted with element velocity
-        write(37,*) radius, radialvelocity, MIN(parent_age,time)
+        cell_velocities(parent,1) = cell_velocities(parent,1) + radialvelocity
+        cell_velocities(parent,2) = cell_velocities(parent,2) + radius
       else
         displacement(:) = elements(n)%position(:) - pre_COM(:)
         radius = SQRT(DOT_PRODUCT(displacement,displacement))
         radialvelocity = DOT_PRODUCT(displacement(:)/radius,elements(n)%velocity(:)) !Unit vector from COM to element dotted with element velocity
-        write(38,*) radius, radialvelocity, MIN(parent_age,time)
+        cell_velocities(parent,1) = cell_velocities(parent,1) + radialvelocity
+        cell_velocities(parent,2) = cell_velocities(parent,2) + radius
+      endif
+    enddo
+
+    do n=1,nc
+      cell_velocities(n,1) = cell_velocities(n,1)/cells(n)%c_elements(0)
+      cell_velocities(n,2) = cell_velocities(n,2)/cells(n)%c_elements(0)
+      age = cells(n)%age
+      if (cells(n)%fate.EQ.1) then
+        write(37,*) cell_velocities(n,2), cell_velocities(n,1), MIN(age,time) !or just age without MIN()?
+      else
+        write(38,*) cell_velocities(n,2), cell_velocities(n,1), MIN(age,time) !or just age without MIN()?
       endif
     enddo
 
