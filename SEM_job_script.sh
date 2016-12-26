6a961e16a03bcd1f09ac0ff5fc1e8ae7ba966eab
Christopher Revell <cr395@cam.ac.uk>
2016-12-26 11:15:50 +0000

    Update to job script

--- SEM_job_script.sh
+++ SEM_job_script.sh
@@ -1,23 +1,43 @@
 #Script to run multiple SEM simulations with different parameters
 
-./ScEM_master 3.0 3.0 0.2 1.0 1.0 1 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 1.0 2 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 1.0 3 > /dev/null
-./ScEM_master 3.0 3.0 0.2 1.0 1.0 4 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 1.0 5 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 1.0 6 > /dev/null
-./ScEM_master 3.0 3.0 0.2 1.0 1.0 7 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 1.0 8 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 1.0 9 > /dev/null
-./ScEM_master 3.0 3.0 0.2 1.5 1.0 1 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.5 1.0 2 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.5 1.0 3 > /dev/null
-./ScEM_master 3.0 3.0 0.2 1.5 1.0 4 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.5 1.0 5 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.5 1.0 6 > /dev/null
-./ScEM_master 3.0 3.0 0.2 1.5 1.0 7 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.5 1.0 8 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.5 1.0 9 > /dev/null
-./ScEM_master 3.0 3.0 0.2 2.0 1.0 1 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 1.0 2 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 1.0 3 > /dev/null
-./ScEM_master 3.0 3.0 0.2 2.0 1.0 4 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 1.0 5 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 1.0 6 > /dev/null
-./ScEM_master 3.0 3.0 0.2 2.0 1.0 7 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 1.0 8 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 1.0 9 > /dev/null
-./ScEM_master 2.0 2.0 0.2 1.0 1.0 1 > /dev/null & ./ScEM_master 2.0 2.0 0.2 1.0 1.0 2 > /dev/null & ./ScEM_master 2.0 2.0 0.2 1.0 1.0 3 > /dev/null
-./ScEM_master 2.0 2.0 0.2 1.0 1.0 4 > /dev/null & ./ScEM_master 2.0 2.0 0.2 1.0 1.0 5 > /dev/null & ./ScEM_master 2.0 2.0 0.2 1.0 1.0 6 > /dev/null
-./ScEM_master 2.0 2.0 0.2 1.0 1.0 7 > /dev/null & ./ScEM_master 2.0 2.0 0.2 1.0 1.0 8 > /dev/null & ./ScEM_master 2.0 2.0 0.2 1.0 1.0 9 > /dev/null
-./ScEM_master 3.0 2.0 0.2 1.0 1.0 1 > /dev/null & ./ScEM_master 3.0 2.0 0.2 1.0 1.0 2 > /dev/null & ./ScEM_master 3.0 2.0 0.2 1.0 1.0 3 > /dev/null
-./ScEM_master 3.0 2.0 0.2 1.0 1.0 4 > /dev/null & ./ScEM_master 3.0 2.0 0.2 1.0 1.0 5 > /dev/null & ./ScEM_master 3.0 2.0 0.2 1.0 1.0 6 > /dev/null
-./ScEM_master 3.0 2.0 0.2 1.0 1.0 7 > /dev/null & ./ScEM_master 3.0 2.0 0.2 1.0 1.0 8 > /dev/null & ./ScEM_master 3.0 2.0 0.2 1.0 1.0 9 > /dev/null
-./ScEM_master 3.0 3.0 0.2 1.0 0.2 1 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 0.2 2 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 0.2 3 > /dev/null
-./ScEM_master 3.0 3.0 0.2 1.0 0.2 4 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 0.2 5 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 0.2 6 > /dev/null
-./ScEM_master 3.0 3.0 0.2 1.0 0.2 7 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 0.2 8 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 0.2 9 > /dev/null
-./ScEM_master 3.0 3.0 0.2 2.0 0.2 1 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 0.2 2 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 0.2 3 > /dev/null
-./ScEM_master 3.0 3.0 0.2 2.0 0.2 4 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 0.2 5 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 0.2 6 > /dev/null
-./ScEM_master 3.0 3.0 0.2 2.0 0.2 7 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 0.2 8 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 0.2 9 > /dev/null
+#./ScEM_master 3.0 3.0 0.2 1.0 1.0 1 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 1.0 2 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 1.0 3 > /dev/null &
+#wait
+#./ScEM_master 3.0 3.0 0.2 1.0 1.0 4 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 1.0 5 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 1.0 6 > /dev/null &
+#wait
+./ScEM_master 3.0 3.0 0.2 1.0 1.0 7 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 1.0 8 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 1.0 9 > /dev/null &
+wait
+./ScEM_master 3.0 3.0 0.2 1.5 1.0 1 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.5 1.0 2 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.5 1.0 3 > /dev/null &
+wait
+./ScEM_master 3.0 3.0 0.2 1.5 1.0 4 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.5 1.0 5 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.5 1.0 6 > /dev/null &
+wait
+./ScEM_master 3.0 3.0 0.2 1.5 1.0 7 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.5 1.0 8 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.5 1.0 9 > /dev/null &
+wait
+./ScEM_master 3.0 3.0 0.2 2.0 1.0 1 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 1.0 2 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 1.0 3 > /dev/null &
+wait
+./ScEM_master 3.0 3.0 0.2 2.0 1.0 4 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 1.0 5 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 1.0 6 > /dev/null &
+wait
+./ScEM_master 3.0 3.0 0.2 2.0 1.0 7 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 1.0 8 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 1.0 9 > /dev/null &
+wait
+./ScEM_master 2.0 2.0 0.2 1.0 1.0 1 > /dev/null & ./ScEM_master 2.0 2.0 0.2 1.0 1.0 2 > /dev/null & ./ScEM_master 2.0 2.0 0.2 1.0 1.0 3 > /dev/null &
+wait
+./ScEM_master 2.0 2.0 0.2 1.0 1.0 4 > /dev/null & ./ScEM_master 2.0 2.0 0.2 1.0 1.0 5 > /dev/null & ./ScEM_master 2.0 2.0 0.2 1.0 1.0 6 > /dev/null &
+wait
+./ScEM_master 2.0 2.0 0.2 1.0 1.0 7 > /dev/null & ./ScEM_master 2.0 2.0 0.2 1.0 1.0 8 > /dev/null & ./ScEM_master 2.0 2.0 0.2 1.0 1.0 9 > /dev/null &
+wait
+./ScEM_master 3.0 2.0 0.2 1.0 1.0 1 > /dev/null & ./ScEM_master 3.0 2.0 0.2 1.0 1.0 2 > /dev/null & ./ScEM_master 3.0 2.0 0.2 1.0 1.0 3 > /dev/null &
+wait
+./ScEM_master 3.0 2.0 0.2 1.0 1.0 4 > /dev/null & ./ScEM_master 3.0 2.0 0.2 1.0 1.0 5 > /dev/null & ./ScEM_master 3.0 2.0 0.2 1.0 1.0 6 > /dev/null &
+wait
+./ScEM_master 3.0 2.0 0.2 1.0 1.0 7 > /dev/null & ./ScEM_master 3.0 2.0 0.2 1.0 1.0 8 > /dev/null & ./ScEM_master 3.0 2.0 0.2 1.0 1.0 9 > /dev/null &
+wait
+./ScEM_master 3.0 3.0 0.2 1.0 0.2 1 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 0.2 2 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 0.2 3 > /dev/null &
+wait
+./ScEM_master 3.0 3.0 0.2 1.0 0.2 4 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 0.2 5 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 0.2 6 > /dev/null &
+wait
+./ScEM_master 3.0 3.0 0.2 1.0 0.2 7 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 0.2 8 > /dev/null & ./ScEM_master 3.0 3.0 0.2 1.0 0.2 9 > /dev/null &
+wait
+./ScEM_master 3.0 3.0 0.2 2.0 0.2 1 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 0.2 2 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 0.2 3 > /dev/null &
+wait
+./ScEM_master 3.0 3.0 0.2 2.0 0.2 4 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 0.2 5 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 0.2 6 > /dev/null &
+wait
+./ScEM_master 3.0 3.0 0.2 2.0 0.2 7 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 0.2 8 > /dev/null & ./ScEM_master 3.0 3.0 0.2 2.0 0.2 9 > /dev/null &
