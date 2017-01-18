ccf741c0761d3810f7977aa09a144c42dbf3a570
Christopher Revell <cr395@cam.ac.uk>
2017-01-18 22:09:16 +0000

    Updates to phase space plotting for adhesion-tension and tension-tension

--- scripts/phasespace_tension-tension.py
+++ scripts/phasespace_tension-tension.py
@@ -11,37 +11,71 @@ measurements = [m for m in os.listdir(os.path.join(argv[1],datafolders[0],"meand
 
 for measurement in measurements:
 
-    phasespace = np.zeros((4,4))
+    phasespace_final = np.zeros((4,4))
+    phasespace_mean = np.zeros((4,4))
+    x = []
+    y = []
 
     for i in enumerate(datafolders):
 
-        x = int(i[1].split("_")[0])
-        y = int(i[1].split("_")[1])
+        if int(i[1].split("_")[1])/10 not in x:
+            x.append(int(i[1].split("_")[1])/100)
+        if int(i[1].split("_")[0])/10 not in y:
+            y.append(int(i[1].split("_")[0])/100)
 
         measurementdata = np.genfromtxt(os.path.join(argv[1],i[1],"meandata",measurement))
         if "neighbours" in measurement:
-            z = np.mean(measurementdata[:,1])
+            z_final = measurementdata[-1,1]
+            z_mean = np.mean(measurementdata[:,1])
         else:
-            z = np.mean(measurementdata[:,3])
+            z_final = measurementdata[-1,3]
+            z_mean = np.mean(measurementdata[:,3])
 
-        phasespace[-(int(int(i[0])/4)+1),int(i[0])%4] = z
+        phasespace_final[-(int(int(i[0])/4)+1),int(i[0])%4] = z_final
+        phasespace_mean[-(int(int(i[0])/4)+1),int(i[0])%4] = z_mean
 
-        fig,ax = plt.subplots()
-        cax = ax.imshow(phasespace,cmap="Reds",extent=[0,1,0,3])
-        if "neighbours" in measurement:
-            ax.set_title("Mean number of additional epiblast-epiblast neighbour pairs\nrelative to random system, averaged over time")
-        if "surface" in measurement:
-            ax.set_title("Mean proportion of external surface occupied by\nprimitive endoderm, averaged over time")
-        if "radius_time" in measurement:
-            ax.set_title("Mean distance of primitive endoderm from centre of mass\nrelative to random system, averaged over time")
-        if "radius_age" in measurement:
-            ax.set_title("Mean distance of primitive endoderm from centre of mass\nrelative to random system, averaged over cell age")
-        ax.set_ylabel("Primitive Endoderm Adhesion Magnitude")
-        ax.set_xlabel("Epiblast Homotypic Interface Tension Factor")
-        ax.set_aspect(1/3)
-        # Add colorbar, make sure to specify tick locations to match desired ticklabels
-        cbar = fig.colorbar(cax, ticks=[np.min(phasespace), np.max(phasespace)])
-        cbar.ax.set_yticklabels([str(np.min(phasespace)), str(np.max(phasespace))])  # vertically oriented colorbar
-        fig.savefig(os.path.join(argv[1],measurement[:-4]+"_phasespace.png"))
-        plt.close(fig)
-        np.savetxt(os.path.join(argv[1],measurement[:-4]+"_phasespace_data.txt"),phasespace)
+    fig,ax = plt.subplots()#figsize=(9,6))
+    cax = ax.imshow(phasespace_final,cmap="Reds",extent=[0.125,1.125,0.5,4.5],interpolation="none")
+    if "neighbours" in measurement:
+        ax.set_title("Final value of number of additional epiblast-epiblast\nneighbour pairs relative to random system")
+    if "surface" in measurement:
+        ax.set_title("Final value of proportion of external surface\noccupied by primitive endoderm")
+    if "radius_time" in measurement:
+        ax.set_title("Final value of distance of primitive endoderm from\ncentre of mass relative to random system")
+    if "radius_age" in measurement:
+        ax.set_title("Final value of distance of primitive endoderm from\ncentre of mass relative to random system")
+    ax.set_ylabel("Epiblast Homotypic Interface Tension Factor")
+    ax.set_xlabel("Epiblast and Primitive Endoderm Heterotypic\nInterface Tension Factor")
+    ax.set_yticks(y)
+    #ax.set_yticklabels(y)
+    ax.set_xticks(x)
+    #ax.set_xticklabels(x)
+    ax.set_aspect(1/3)
+    cbar = fig.colorbar(cax, ticks=[np.min(phasespace_final), np.max(phasespace_final)])
+    cbar.ax.set_yticklabels([str(np.min(phasespace_final)), str(np.max(phasespace_final))])  # vertically oriented colorbar
+    fig.set_tight_layout(True)
+    fig.savefig(os.path.join(argv[1],measurement[:-4]+"_phasespace_finalvalue.png"))
+    plt.close(fig)
+
+    fig,ax = plt.subplots()#figsize=(9,6))
+    cax = ax.imshow(phasespace_mean,cmap="Reds",extent=[0.125,1.125,0.5,4.5],interpolation="none")
+    if "neighbours" in measurement:
+        ax.set_title("Mean over time of number of additional epiblast-epiblast\nneighbour pairsrelative to random system")
+    if "surface" in measurement:
+        ax.set_title("Mean over time of proportion of external surface\noccupied by primitive endoderm")
+    if "radius_time" in measurement:
+        ax.set_title("Mean over time of distance of primitive endoderm from\ncentre of massrelative to random system")
+    if "radius_age" in measurement:
+        ax.set_title("Mean over cell age of distance of primitive endoderm\nfrom centre of mass relative to random system")
+    ax.set_ylabel("Epiblast Homotypic Interface Tension Factor")
+    ax.set_xlabel("Epiblast and Primitive Endoderm Heterotypic\nInterface Tension Factor")
+    ax.set_yticks(y)
+    #ax.set_yticklabels(y)
+    ax.set_xticks(x)
+    #ax.set_xticklabels(x)
+    ax.set_aspect(1/3)
+    cbar = fig.colorbar(cax, ticks=[np.min(phasespace_mean), np.max(phasespace_mean)])
+    cbar.ax.set_yticklabels([str(np.min(phasespace_mean)), str(np.max(phasespace_mean))])  # vertically oriented colorbar
+    fig.set_tight_layout(True)
+    fig.savefig(os.path.join(argv[1],measurement[:-4]+"_phasespace_mean.png"))
+    plt.close(fig)
