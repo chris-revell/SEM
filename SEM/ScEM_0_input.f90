438d5f4148138f354080d9473e08f46af90fde2a
Christopher Revell <cr395@cam.ac.uk>
2017-06-02 20:18:24 +0100

    Fixing conflicts

--- SEM/ScEM_0_input.f90
+++ SEM/ScEM_0_input.f90
@@ -58,10 +58,10 @@ module scem_0_input
   !Variables for setting output folder
 !  character(len=8) :: date_of_run   !Date of simulation run
 !  character(len=10):: time_of_run   !Time of simulation run
-  character(len=29):: output_folder !Name of folder created for data output, labelled according to date and time of run.
+  character(len=25):: output_folder !Name of folder created for data output, labelled according to date and time of run.
   !Variables defined for command line input
-  character(len=4) :: arg1,arg2,arg3,arg4,arg5
-  character(len=1) :: arg6
+  character(len=4) :: arg1,arg2,arg3,arg4
+  character(len=1) :: arg5
   logical :: randomising
   logical :: intro
 
@@ -101,11 +101,11 @@ module scem_0_input
       flag_randomise  = 1 ! When importing initial system setup from file, if flag_randomise=1, the program will assign fates to the imported cells randomly rather than keeping the initial fate distribution
 
       !Output control switches
-      flag_povray = 1                ! Switch to turn off povray output entirely
+      flag_povray = 0                ! Switch to turn off povray output entirely
         flag_povray_volumes      = 0 ! flag_povray_volumes = 1 to output cell position data in povray format, 0 to skip.
         flag_povray_elements     = 0 ! flag_povray_elements = 1 to output element position data in povray format, 0 to skip.
         flag_povray_pairs        = 0 ! flag_povray_pairs = 1 to show interaction pairs as cylinders in povray output, 0 to skip.
-        flag_povray_triangles    = 1 ! Switch to turn smoothed triangle povray output on and off.
+        flag_povray_triangles    = 0 ! Switch to turn smoothed triangle povray output on and off.
         flag_povray_cortex_pairs = 0 ! Switch to turn Delaunay cortex interaction on and off
       flag_count_output       = 0    ! Switch to turn off outputting cell count
       flag_fate_output        = 0    ! Switch to turn off outputting cell fate data
@@ -118,7 +118,7 @@ module scem_0_input
       flag_measure_surface    = 1    ! Switch to turn off surface sorting measurement
       flag_measure_velocity   = 0    ! Switch to turn off velocity measurement
       flag_measure_com        = 0
-      flag_measure_randomised = 1    ! Switch for subroutine that randomises fates in system and takes measurements as a baseline comparison
+      flag_measure_randomised = 0    ! Switch for subroutine that randomises fates in system and takes measurements as a baseline comparison
 
       !Simulation control parameters
       nc_initial        = 10
@@ -137,10 +137,11 @@ module scem_0_input
       DIT_response(1,0) = 1.0 ! Epiblast external system surface DIT response factor
       CALL GET_COMMAND_ARGUMENT(4,arg4)
       READ(arg4,*) DIT_response(1,1) ! Epiblast homotypic interface DIT response factor
+      DIT_response(1,0) = 1.0 ! Epiblast external system surface DIT response factor
+      DIT_response(1,1) = 1.0 ! Epiblast homotypic interface DIT response factor
       DIT_response(1,2) = 1.0 ! Epiblast heterotypic interface DIT response factor
       DIT_response(2,0) = 1.0 ! Primitive endoderm external system surface DIT response factor
-      CALL GET_COMMAND_ARGUMENT(5,arg5)
-      READ(arg5,*) DIT_response(2,1)  ! Primitive endoderm homotypic interface DIT response factor
+      DIT_response(2,1) = 1.0 ! Primitive endoderm homotypic interface DIT response factor
       DIT_response(2,2) = 1.0 ! Primitive endoderm heterotypic interface DIT response factor
 
       ! *** Everything from here on can effectively be ignored for the purposes of testing simulation parameters ***
@@ -165,10 +166,10 @@ module scem_0_input
 
       !Create labelled file for data output
       !Catch date and time, create folder to store data in
-      CALL GET_COMMAND_ARGUMENT(6,arg6)
+      CALL GET_COMMAND_ARGUMENT(5,arg5)
       !call date_and_time(DATE=date_of_run,TIME=time_of_run)
-      output_folder = "../data/"//arg1(1:1)//arg1(3:4)//"_"//arg2(1:1)//arg2(3:4)//"_"//arg3(1:1)//arg3(3:4)//"_"//&
-        arg4(1:1)//arg4(3:4)//"_"//arg5(1:1)//arg5(3:4)//"_"//arg6(1:1)
+      output_folder = "../data/"//arg1(1:1)//arg1(3:4)//"_"//arg2(1:1)//arg2(3:4)//"_"//arg3(1:1)//arg3(3:4)//"_"&
+        //arg4(1:1)//arg4(3:4)//"_"//arg5(1:1)
       call system("mkdir "//output_folder)
       call system("mkdir "//output_folder//"/system_data")
       call system("mkdir "//output_folder//"/sorting_data")
